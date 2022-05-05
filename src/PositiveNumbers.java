9038b98e74173709420434285e0a2da6876c1619
rblyimer <101184162+rblyimer@users.noreply.github.com>
2022-05-05 15:23:32 -0400

    second commit

--- src/PositiveNumbers.java
+++ src/PositiveNumbers.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 public class PositiveNumbers {
     public static void main(String[] args) throws Exception {
     Scanner scanner = new Scanner(System.in);
-int num = 0;
 while (true) {
     System.out.println("Enter a number:");
     int number = Integer.valueOf(scanner.nextLine());
@@ -15,7 +14,6 @@ while (true) {
     }
     System.out.println("Number is " + number);
 }
-System.out.println("\n" + "Process finished with exit code " + num);
 scanner.close();
 }
 }
